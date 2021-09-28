bf191ac87b5f0ae73b652e51462bed86a5a21928
selva3003 <30857531+selva3003@users.noreply.github.com>
2021-09-28 21:33:02 +0530

    Create Patterns.js

---
+++ src/Patterns.js
@@ -0,0 +1,8 @@
+/* 
+HOC
+  1. We go for higher order components when we need the common functinality that needs to be shared across the componets. 
+  2. its basically a function that takes a component as an argument and returns a new component with extra functionalities. 
+Render Props
+  
+  
+*/
